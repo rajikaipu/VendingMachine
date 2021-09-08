e5f5791f4d8dd40cbe91b17796178749b14f0f51
rajikaipu <rl.kaipu@gmail.com>
2021-09-08 21:25:36 +0100

    Add files via upload

---
+++ vendingmachine/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace vendingmachine1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtAddCoins = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.richTxtInsertCoins = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rejectedCoins = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBuy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(185, 195);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Visible = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtAddCoins
+            // 
+            this.txtAddCoins.Location = new System.Drawing.Point(408, 168);
+            this.txtAddCoins.Name = "txtAddCoins";
+            this.txtAddCoins.Size = new System.Drawing.Size(81, 20);
+            this.txtAddCoins.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(408, 194);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(61, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // richTxtInsertCoins
+            // 
+            this.richTxtInsertCoins.Location = new System.Drawing.Point(325, 0);
+            this.richTxtInsertCoins.Name = "richTxtInsertCoins";
+            this.richTxtInsertCoins.Size = new System.Drawing.Size(194, 96);
+            this.richTxtInsertCoins.TabIndex = 0;
+            this.richTxtInsertCoins.Text = "INSERT COINS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(347, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Add Coins";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(300, 230);
+            this.splitter1.TabIndex = 3;
+            this.splitter1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Item,
+            this.price});
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(245, 153);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Item
+            // 
+            this.Item.HeaderText = "Item";
+            this.Item.Name = "Item";
+            this.Item.ReadOnly = true;
+            // 
+            // price
+            // 
+            this.price.HeaderText = "Price";
+            this.price.Name = "price";
+            this.price.ReadOnly = true;
+            // 
+            // rejectedCoins
+            // 
+            this.rejectedCoins.Location = new System.Drawing.Point(408, 117);
+            this.rejectedCoins.Name = "rejectedCoins";
+            this.rejectedCoins.Size = new System.Drawing.Size(111, 24);
+            this.rejectedCoins.TabIndex = 5;
+            this.rejectedCoins.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(322, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Returned Coins";
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Location = new System.Drawing.Point(24, 191);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(77, 25);
+            this.btnBuy.TabIndex = 7;
+            this.btnBuy.Text = "Buy";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 230);
+            this.Controls.Add(this.btnBuy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rejectedCoins);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTxtInsertCoins);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAddCoins);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Vending Machine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtAddCoins;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.RichTextBox richTxtInsertCoins;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Item;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.RichTextBox rejectedCoins;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBuy;
+    }
+}
+
---
+++ vendingmachine/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+
+namespace vendingmachine1
+{
+    public partial class Form1 : Form
+    {
+        private static ArrayList ListItems = new ArrayList(new String[] { "Cola", "Crisps", "Chacolate", "Water" });
+        private static ArrayList ListPrice = new ArrayList(new Double[] { 1.00, 0.50, 0.65, 0.90 });
+        private static ArrayList AllowedCoins = new ArrayList(new Double[] { 0.05, 0.10, 0.20, 0.50, 1.0, 2.0 });
+        private static double total = 0.00;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ListItems.Count; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dataGridView1);
+                row.Cells[0].Value = ListItems[i];
+                row.Cells[1].Value = ListPrice[i];
+                dataGridView1.Rows.Add(row);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            richTxtInsertCoins.Text = "Selected Item " + ListItems[e.RowIndex];
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtAddCoins.Text == null || txtAddCoins.Text.Trim() == "")
+            {
+                richTxtInsertCoins.Text = richTxtInsertCoins.Text + "\n" + "Insert Coin and then press Add";
+            } else
+            {
+                String amt = txtAddCoins.Text.Replace("£", "");
+                Double val = 0.00;
+                if(amt.Contains("p") || amt.Contains("P"))
+                {
+                    amt = amt.Replace("p", "");
+                    amt = amt.Replace("P", "");
+                    Double v = Convert.ToDouble(amt);
+                    val = v / 100;
+                }
+                else
+                {
+                    val = Convert.ToDouble(amt);
+                }
+                if(AllowedCoins.Contains(val))
+                {
+                    total = total + val; ;
+                    richTxtInsertCoins.Text = "TOTAL : " + total.ToString();
+                } else
+                {
+                    rejectedCoins.Text = rejectedCoins.Text + txtAddCoins.Text + "\n";
+                }
+            }
+
+
+        }
+
+        private void btnBuy_Click(object sender, EventArgs e)
+        {
+            String item = (String) ListItems[dataGridView1.SelectedCells[0].RowIndex];
+            Double price = (Double)ListPrice[dataGridView1.SelectedCells[0].RowIndex];
+            if(price > total)
+            {
+                richTxtInsertCoins.Text = "PRICE : " + price.ToString();
+            } else
+            {
+                richTxtInsertCoins.Text = "THANK YOU. \nHAVE A GOOD DAY !!";
+                
+                rejectedCoins.Text = Convert.ToDouble(txtAddCoins.Text) - price + "\n";
+                btnReset.Visible = true;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            richTxtInsertCoins.Text = "INSERT COINS";
+            total = 0.00;
+        }
+    }
+}
